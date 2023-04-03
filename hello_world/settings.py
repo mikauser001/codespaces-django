200178fdd543bd14e98373813404f1a737b28691
mika <88951115+mikauser001@users.noreply.github.com>
2023-04-03 16:54:19 +0000

    models.py

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_browser_reload",
+    "user", 
+    "quiz",
 ]
 
 MIDDLEWARE = [
@@ -88,6 +90,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'user.CustomUser'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
